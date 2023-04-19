48c99eaac6c3e05e82a9e22458334cf30f2416c8
caterinanauta <caterinanauta@gmail.com>
2023-04-19 15:33:28 -0300

    javaEntrega

--- pages/app.js
+++ pages/app.js
@@ -1,29 +1,42 @@
-let item;
-let suma = 0;
-let promedio;
+let usuario;
+let codigoPostal;
+let direccionDeEnvio;
+let precioItem;
+let nombreItem;
 let cantidad;
-
-cantidad= Number(prompt("Cuantos items desea ingresar"));
-
-for (let i = 1 ; i <= cantidad ; i++)
-{
-precioItem = Number(prompt ("Ingrese precio"));
-console.log("acumulandose...:" + suma);
-suma = suma + precioItem;
-}
-
-promedio = suma / cantidad;
-
-console.log("el promedio es " + promedio);
-
-let continuar = "si";
-
-while (continuar != "basta")
-{
-    console.log ("Agregando un nuevo item al carrito de compras..");
-    continuar = prompt("Desea agregar otro item? si/basta");
-}
-
-console.log("Resto del codigo...");
+let total = 0;
+let opcion;
 
 
+alert('Bienvenido al carrito de compras de YuKa');
+usuario = prompt('Ingrese su usuario');
+codigoPostal = prompt('Ingrese su Codigo Postal');
+direccionDeEnvio = prompt('Ingrese la direccion de entrega');
+
+while (usuario == "" || codigoPostal == "" || direccionDeEnvio == ""){
+    alert('No se registro algun dato, vuelva a intentarlo');
+    usuario = prompt('Ingrese su usuario');
+    codigoPostal = prompt('Ingrese su Codigo Postal');
+    direccionDeEnvio = prompt('Ingrese la direccion de entrega');
+} alert ("Datos registrados");
+
+do{
+   nombreItem = prompt('ingrese nombre del producto que desea comprar');
+   precioItem = Number(prompt('Ingrese su precio'));
+
+
+   while (precioItem < 0) {
+    precioItem = parseInt(prompt('Precio invalido, ingrese otro')); 
+   }
+
+    cantidad = parseInt(prompt('ingrese cantidad'));
+
+    while (cantidad <= 0) {
+        cantidad = parseInt(prompt('cantidad invalida, ingrese otra'));
+    }
+         total = total + cantidad*precioItem; 
+    
+    opcion = prompt('Desea ingresar otro item?');
+   }while(opcion =='si');
+
+alert('el total para ' + usuario + ' es de $' + total);
